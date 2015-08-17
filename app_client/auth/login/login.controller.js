79f4499401ab57ed2556fb45e994ac07006664ca
Simon Holmes <sholmes@liveperson.com>
2015-08-17 07:09:42 +0100

    Enable register and login controllers

--- app_client/auth/login/login.controller.js
+++ app_client/auth/login/login.controller.js
@@ -1,42 +1,28 @@
 (function () {
 
   angular
-    .module('meanApp')
-    .controller('loginCtrl', loginCtrl);
+  .module('meanApp')
+  .controller('loginCtrl', loginCtrl);
 
-  loginCtrl.$inject = ['$location'];
-  function loginCtrl($location) {
+  loginCtrl.$inject = ['$location', 'authentication'];
+  function loginCtrl($location, authentication) {
     var vm = this;
 
-    // vm.credentials = {
-    //   email : "",
-    //   password : ""
-    // };
+    vm.credentials = {
+      email : "",
+      password : ""
+    };
 
-    // vm.returnPage = $location.search().page || '/';
-
-    // vm.onSubmit = function () {
-    //   vm.formError = "";
-    //   if (!vm.credentials.email || !vm.credentials.password) {
-    //     vm.formError = "All fields required, please try again";
-    //     return false;
-    //   } else {
-    //     vm.doLogin();
-    //   }
-    // };
-
-    // vm.doLogin = function() {
-    //   vm.formError = "";
-    //   authentication
-    //     .login(vm.credentials)
-    //     .error(function(err){
-    //       vm.formError = err;
-    //     })
-    //     .then(function(){
-    //       $location.search('page', null); 
-    //       $location.path(vm.returnPage);
-    //     });
-    // };
+    vm.onSubmit = function () {
+      authentication
+      .login(vm.credentials)
+      .error(function(err){
+        alert(err);
+      })
+      .then(function(){
+        $location.path('profile');
+      });
+    };
 
   }
 
--- app_client/auth/register/register.controller.js
+++ app_client/auth/register/register.controller.js
@@ -4,44 +4,26 @@
     .module('meanApp')
     .controller('registerCtrl', registerCtrl);
 
-  registerCtrl.$inject = ['$location'];
-  function registerCtrl($location) {
+  registerCtrl.$inject = ['$location', 'authentication'];
+  function registerCtrl($location, authentication) {
     var vm = this;
 
-    // vm.pageHeader = {
-    //   title: 'Create a new Loc8r account'
-    // };
+    vm.credentials = {
+      name : "",
+      email : "",
+      password : ""
+    };
 
-    // vm.credentials = {
-    //   name : "",
-    //   email : "",
-    //   password : ""
-    // };
-
-    // vm.returnPage = $location.search().page || '/';
-
-    // vm.onSubmit = function () {
-    //   vm.formError = "";
-    //   if (!vm.credentials.name || !vm.credentials.email || !vm.credentials.password) {
-    //     vm.formError = "All fields required, please try again";
-    //     return false;
-    //   } else {
-    //     vm.doRegister();
-    //   }
-    // };
-
-    // vm.doRegister = function() {
-    //   vm.formError = "";
-    //   authentication
-    //     .register(vm.credentials)
-    //     .error(function(err){
-    //       vm.formError = err;
-    //     })
-    //     .then(function(){
-    //       $location.search('page', null); 
-    //       $location.path(vm.returnPage);
-    //     });
-    // };
+    vm.onSubmit = function () {
+      authentication
+        .register(vm.credentials)
+        .error(function(err){
+          alert(err);
+        })
+        .then(function(){
+          $location.path('profile');
+        });
+    };
 
   }
 
